98bb967f1ef53bb4c985fd3c14a60e6bc50cf541
Andy Castle <masterchief_and@live.com>
2025-02-06 16:11:14 -0500

    Se coloco el metodo para obtener la geolocalizacion del usuario

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-screen',
   templateUrl: './map-screen.component.html',
-  styleUrls: ['./map-screen.component.css']
+  styleUrls: ['./map-screen.component.css'],
 })
 export class MapScreenComponent {
-
+  constructor(private placesService: PlacesService) {}
 }
