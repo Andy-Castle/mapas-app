716d1c41c207246770c41e86f66d65ba6604aeaf
Andy Castle <masterchief_and@live.com>
2025-02-07 11:10:36 -0500

    Debounce Manual, se hizo un metodo para esperar que el usuario deje de escribir para que este mande la petición(que aun no esta hecha)

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
+  private debounceTimer?: NodeJS.Timeout;
 
+  constructor() {}
+
+  onQueryChanged(query: string = '') {
+    // console.log(query);
+    if (this.debounceTimer) clearTimeout(this.debounceTimer);
+
+    this.debounceTimer = setTimeout(() => {
+      console.log('Mandar este query: ', query);
+    }, 350);
+  }
 }
